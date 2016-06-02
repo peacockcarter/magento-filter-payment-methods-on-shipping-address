e20abac7bda93d65bee8332b23c76843d30e6d41
richie <richiemcfarlane@gmail.com>
2016-06-02 18:48:36 +0100

    Bugfix - Config error, change class path
    Containing folder previously renamed but classes were not.

--- app/code/community/PeacockCarter/FilterPaymentsOnShippingAddress/Model/Observer.php
+++ app/code/community/PeacockCarter/FilterPaymentsOnShippingAddress/Model/Observer.php
@@ -25,7 +25,7 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class Peacock_FilterPaymentMethodsOnShippingAddress_Model_Observer
+class PeacockCarter_FilterPaymentMethodsOnShippingAddress_Model_Observer
 {
     /**
      * @array
